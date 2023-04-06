0e99af35c199c111b73c9aa0bb2e4f9b8b1471c0
Viktoriia Volkova <Viktoria.Volkova@Sedex.com>
2023-04-06 13:18:46 +0200

    Format files

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const chatEl = document.getElementById('chat');
-const ws = new WebSocket('ws://127.0.0.1:8000');
 const formEl = document.getElementById('messageForm');
+const ws = new WebSocket('ws://127.0.0.1:8000');
 
 ws.onmessage = (message) => {
   const messages = JSON.parse(message.data);
@@ -18,8 +18,8 @@ ws.onmessage = (message) => {
 const send = (event) => {
   event.preventDefault();
 
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
+  const name = document.getElementById('name')?.value;
+  const message = document.getElementById('message')?.value;
 
   ws.send(
     JSON.stringify({
